e82ba65312901bb4c0110394fcb828549dd0a863
Mike Starina <x@pnhd.ru>
2023-02-03 14:27:04 +0300

    deploy ready

--- src/models/order.ts
+++ src/models/order.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export interface IOrder {
-    order_number: number;
+    order_key: String;
     owner_name: string;
     owner_phone: string;
     owner_email: string;
@@ -16,21 +16,15 @@ export interface IOrder {
     comment: string;
     order_details: [
         {
-            textile_item: mongoose.Schema.Types.ObjectId;
+            //textile: mongoose.Schema.Types.ObjectId;
+            textile: String;
             qty: number;
+            item_price: number;
             print?: {
                 front_print?: string;
-                front_preview?: string;
-                front_print_params?: string;
                 back_print?: string;
-                back_preview?: string;
-                back_print_params?: string;
                 lsleeve_print?: string;
-                lsleeve_preview?: string;
-                lsleeve_print_params?: string;
                 rsleeve_print?: string;
-                rsleeve_preview?: string;
-                rsleeve_print_params?: string;
             };
         },
     ];
@@ -45,7 +39,7 @@ export interface IOrder {
 
 
 const orderSchema = new mongoose.Schema<IOrder>({
-    order_number: { type: Number, required: false},
+    order_key: { type: String, required: false},
     owner_name: { type: String, minlength: 2, maxlength: 40, required: false },
     owner_phone: { type: String, required: false },
     owner_email: { type: String, required: false },
@@ -60,26 +54,21 @@ const orderSchema = new mongoose.Schema<IOrder>({
     comment: { type: String},
     order_details: [
         {
-            textile_item: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'product',
+            textile: {
+                type: String,
+                //ref: 'product',
             },
             qty: {
               type: Number,
             },
+            item_price: {
+              type: Number,
+            },
             print: {
               front_print: { type: String },
-              front_preview: { type: String },
-              front_print_params: { type: String },
               back_print: { type: String },
-              back_preview: { type: String },
-              back_print_params: { type: String },
               lsleeve_print: { type: String },
-              lsleeve_preview: { type: String },
-              lsleeve_print_params: { type: String },
               rsleeve_print: { type: String },
-              rsleeve_preview: { type: String },
-              rsleeve_print_params: { type: String },
             },
         },
     ],
