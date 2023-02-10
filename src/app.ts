8491b10e0a5a5f63a215c64fb82898bebf5b9363
Mike Starina <x@pnhd.ru>
2023-02-10 22:29:43 +0300

    new db auth

--- src/app.ts
+++ src/app.ts
@@ -52,7 +52,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + '/public')));
 
 mongoose.set('strictQuery', true);
-mongoose.connect(DBURL);
+mongoose.connect(DBURL, { dbName: 'studio' });
 
 
 app.use(requestLogger);
