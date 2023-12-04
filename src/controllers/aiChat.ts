5160fff3d5ed249f972bcf5bd436847de664ef6a
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2023-12-04 21:46:14 +0100

    feat: added before test

--- src/controllers/aiChat.ts
+++ src/controllers/aiChat.ts
@@ -6,17 +6,39 @@ const ENV = dotenv.config();
 const AI_CHAT = ENV.parsed!.AI_CHAT.toString();
 const AI_KEY = ENV.parsed!.AI_KEY.toString();
 
+const uploadImage = (data: any) => {
+  // console.log('>>>', data, '<<<')
+  fetch(data, {
+    method: 'GET',
+    headers: {
+      'Accept': 'image/png',
+      'Content-type': 'image/png',
+    },
+  })
+    // .then((res) => res.headers.)
+    .then((res) => {
+      // console.log('working&&&&&&')
+      const tt = res;
+      console.log(tt);
+      return tt;
+    })
+    .catch((res) => console.log(res, 'ERORRRRRRRRRR'))
+};
+
 export const aiChat = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
 
+  const { words }: any = await req.body;
+
   const body = JSON.stringify({
     "model": "dall-e-3",
-    "prompt": "гном",
+    "prompt": words,
     "n": 1,
-    "size": "1024x1024"
+    "size": "1024x1024",
+    "response_format": "b64_json"
   })
 
   try {
@@ -34,8 +56,15 @@ export const aiChat = async (
     if (image.status === 404) {
       return res.status(404).json({ error: "AI CHAT ERROR" });
     }
-    console.log(data.data)
-    return res.json(data.data[0].url);
+
+    const bJson = data.data[0].b64_json;
+
+    console.log('data>>>>>>', bJson, '<<<<<data')
+    const url = data.data[0].url
+    const dat: any = res.json(url);
+    // uploadImage(url);
+
+    // return uploadImage(dat);
   } catch (err) {
     res.status(500).json(err);
   }
