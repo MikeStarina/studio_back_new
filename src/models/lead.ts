609715cd145120c85fd30ab1de4847b398efcb95
Mike Starina <x@pnhd.ru>
2024-04-09 18:21:17 +0300

    create leads @ bitrix added

--- src/models/lead.ts
+++ src/models/lead.ts
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 interface ILead {
   name: string;
   phone: string;
+  roistat: string;
 }
 
 const leadSchema = new mongoose.Schema<ILead>({
   name: { type: String, required: true, minlength: 2, maxlength: 40 },
   phone: { type: String, required: true },
+  roistat: { type: String, required: false },
 });
 
 
--- src/validator/validator.ts
+++ src/validator/validator.ts
@@ -4,5 +4,6 @@ export const validatorLead = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     phone: Joi.string().min(2).max(12),
+    roistat: Joi.string(),
   }),
 });
