baed3373b291f888c56aaadc698ab03ff5dff38c
Mike Starina <x@pnhd.ru>
2024-05-26 16:05:18 +0300

    temp fix cdek token;

--- src/controllers/deal-add-hook.ts
+++ src/controllers/deal-add-hook.ts
@@ -1,15 +1,38 @@
 import { Request, Response, NextFunction } from "express";
 import ServerError from "../utils/server-error-class";
-
+import fetch from "node-fetch";
 
 
 export const dealAddHook = async (req: Request, res: Response, next: NextFunction) => {
 
-  const data = req.body;
-  console.log(data)
+  const dealId = await req.body.data.FIELDS.ID;
+  try {
+    if (!dealId) {res.status(500).send({ message: 'wrong data' });};
+
+    const chatIdres = await fetch(`https://studio.bitrix24.ru/rest/1/s3r64dqub19xled8/imopenlines.crm.chat.get.json?CRM_ENTITY_TYPE=deal&CRM_ENTITY=${dealId}`).then(res => res.json());
+    const chatId = chatIdres.result[0].CHAT_ID;
+  }
+  catch(e) {
+    next(e);
+  }
 
 
   res.status(200).send({ message: 'ok' });
 
 
 }
+
+
+
+/* {
+  event: 'ONCRMDEALADD',
+  data: { FIELDS: { ID: '9678' } },
+  ts: '1715086657',
+  auth: {
+    domain: 'studio.bitrix24.ru',
+    client_endpoint: 'https://studio.bitrix24.ru/rest/',
+    server_endpoint: 'https://oauth.bitrix.info/rest/',
+    member_id: '5b7d6464a7bfa19dbe6c3cdd5e5c1298',
+    application_token: 't5ukj5gdwwpu8ae59u9iac0ckgpqne0j'
+  }
+} */
