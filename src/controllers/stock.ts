588c86d610396751ef1fc7f2900b13c62b628c33
Mike Strn <x@pnhd.ru>
2025-01-27 11:29:30 +0300

    zzz

--- src/controllers/stock.ts
+++ src/controllers/stock.ts
@@ -11,41 +11,41 @@ export const stockController = async (req: Request, res: Response, next: NextFun
 
     const headers = req.headers;
     const body = await req.body;
-    console.log(req.headers);
-    //console.log(body);
+    //console.log(req.headers);
+
 
     try {
       if (headers.authorization !== `Bearer ${STOCK_TOKEN}`) {
         return res.status(401).send({ message: 'unauthorized' })
       }
 
-      const data = body["Данные"];
-      data.forEach(async (item: any) => {
-        if (item["НаименованиеНоменклатуры"].includes('BASED') || item["НаименованиеНоменклатуры"].includes('Кепка') || item["НаименованиеНоменклатуры"].includes('Шоппер')) {
-            const sizesArr = [{
-              name: "ONE SIZE",
-              qty: item["Количество"]
-            }]
-            await product.updateMany({ oneCCode: item["КодНоменклатуры"] }, { sizes: sizesArr});
-            return
-        }
+      // const data = body["Данные"];
+      // data.forEach(async (item: any) => {
+      //   if (item["НаименованиеНоменклатуры"].includes('BASED') || item["НаименованиеНоменклатуры"].includes('Кепка') || item["НаименованиеНоменклатуры"].includes('Шоппер')) {
+      //       const sizesArr = [{
+      //         name: "ONE SIZE",
+      //         qty: item["Количество"]
+      //       }]
+      //       await product.updateMany({ oneCCode: item["КодНоменклатуры"] }, { sizes: sizesArr});
+      //       return
+      //   }
 
-        //let updatedItem = await product.find({ oneCCode: item["КодНоменклатуры"] });
+      //   //let updatedItem = await product.find({ oneCCode: item["КодНоменклатуры"] });
 
-        if (true) {
-          let sizesArr: Array<{ name: string, qty: number}> = [];
+      //   if (true) {
+      //     let sizesArr: Array<{ name: string, qty: number}> = [];
 
-          item["Характеристики"].forEach((size: any) => {
-            sizesArr.push({
-                name: size["НаименованиеХарактеристики"],
-                qty: size["Количество"]
-            })
-          });
-          await product.updateMany({ oneCCode: item["КодНоменклатуры"] }, { sizes: sizesArr });
-          return
-        }
+      //     item["Характеристики"].forEach((size: any) => {
+      //       sizesArr.push({
+      //           name: size["НаименованиеХарактеристики"],
+      //           qty: size["Количество"]
+      //       })
+      //     });
+      //     await product.updateMany({ oneCCode: item["КодНоменклатуры"] }, { sizes: sizesArr });
+      //     return
+      //   }
 
-      })
+      // })
 
 
       return res.status(200).send({ message: "ok" });
